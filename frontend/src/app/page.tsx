14fb9359663129ece176984a468460e574cc3bfe
tamaki8021 <hbk2002320@gmail.com>
2023-08-17 22:27:04 +0900

    feat: アバターを適当に変更できるコンポーネントの作成

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,15 @@
 'use client';
 
 import { Link } from '@chakra-ui/next-js';
+import { Avatar } from '@/components/Toppage';
 
 export default function Home() {
   return (
-    <Link href="/" color="blue" _hover={{ color: 'blue.500' }}>
-      aaaa
-    </Link>
+    <>
+      <Link href="/" color="blue" _hover={{ color: 'blue.500' }}>
+        aaaa
+      </Link>
+      <Avatar />
+    </>
   );
 }
