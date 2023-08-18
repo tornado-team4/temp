b8a154bb28958dad824a41f83b00de0696e66f2d
tamaki8021 <hbk2002320@gmail.com>
2023-08-18 16:16:38 +0900

    refactor: Avatar componentの移動

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,15 +1,11 @@
 'use client';
 
 import { Link } from '@chakra-ui/next-js';
-import { Avatar } from '@/components/Toppage';
 
 export default function Home() {
   return (
-    <>
-      <Link href="/" color="blue" _hover={{ color: 'blue.500' }}>
-        aaaa
-      </Link>
-      <Avatar />
-    </>
+    <Link href="/" color="blue" _hover={{ color: 'blue.500' }}>
+      aaaa
+    </Link>
   );
 }
