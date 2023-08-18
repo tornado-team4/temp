7daf94b947a1247bc2b4130797553486b53cc139
ltoppyl <toppyajibutu@gmail.com>
2023-08-18 16:48:15 +0900

    test: DB との接続を含む簡単なテストの作成

--- backend/test_main.py
+++ backend/test_main.py
@@ -9,3 +9,15 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == "Hello World v0.0!"
+
+
+def test_read_avatar_list():
+    response = client.get("/api/v1/avatar-list")
+    assert response.status_code == 200
+    assert response.json() == [
+        "https://bit.ly/prosper-baba",
+        "https://bit.ly/dan-abramov",
+        "https://bit.ly/kent-c-dodds",
+        "https://bit.ly/ryan-florence",
+        "https://bit.ly/code-beast",
+    ]
