faa5c765908bb328602e2875a6d1a3a832e9a0dc
ltoppyl <toppyajibutu@gmail.com>
2023-08-19 09:21:58 +0900

    feat: user の追加時に役割の概念の追加

--- backend/cruds/create/create_room.py
+++ backend/cruds/create/create_room.py
@@ -4,9 +4,10 @@ def create_room(db, name, avatar_url):
 
     users_collection_ref = room_collection_ref.document(room_doc_id).collection("users")
     user_doc_ref = users_collection_ref.document()
-    user_doc_ref.set({"name": name, "avatar": avatar_url})
+    user_doc_ref.set({"role": "host", "name": name, "avatar": avatar_url})
 
     return {
         "room_id": room_doc_id,
         "user_id": user_doc_ref.id,
+        "role": "host"
     }
