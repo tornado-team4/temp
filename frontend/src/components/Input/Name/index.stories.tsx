07a1001e5f156a910aa5b937159979d75e3e9728
tamaki8021 <hbk2002320@gmail.com>
2023-08-21 00:31:34 +0900

    feat: Inputのコンポーネントを作成

---
+++ frontend/src/components/Input/Name/index.stories.tsx
@@ -0,0 +1,18 @@
+// InputName.stories.ts|tsx
+
+import type { Meta, StoryObj } from '@storybook/react';
+import { InputName } from './index';
+
+const meta: Meta<typeof InputName> = {
+  title: 'Input/InputName',
+  component: InputName,
+};
+
+export default meta;
+type Story = StoryObj<typeof InputName>;
+
+export const Default: Story = {
+  args: {
+    borderColor: '#56C1FC',
+  },
+};
