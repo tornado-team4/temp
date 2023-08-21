c9383ccf8040b75e4b4b92b4a86998320a3181f5
NewNomad <tosiki1997@gmail.com>
2023-08-21 16:33:10 +0900

    feat: ピースのframer-motion関連設定

--- frontend/src/app/ingame/components/elements/Puzzle/Piece.tsx
+++ frontend/src/app/ingame/components/elements/Puzzle/Piece.tsx
@@ -1,14 +1,22 @@
 'use client';
 
-import React, { CSSProperties } from 'react';
-
-const styles: CSSProperties = {
-  position: 'absolute',
-  width: '100px',
-  height: '100px',
-  backgroundColor: 'red',
-};
+import React from 'react';
+import { motion } from 'framer-motion';
 
 export const Piece = () => {
-  return <div style={styles}>Piece</div>;
+  return (
+    <motion.div
+      drag
+      dragSnapToOrigin
+      dragElastic={1}
+      dragTransition={{ bounceStiffness: 100, bounceDamping: 12 }}
+      whileHover={{ scale: 1.1 }}
+      whileDrag={{ scale: 1 }}
+      style={{
+        backgroundColor: 'red',
+        width: 100,
+        height: 100,
+      }}
+    ></motion.div>
+  );
 };
--- frontend/src/app/ingame/components/layouts/PuzzlePieceContainer.tsx
+++ frontend/src/app/ingame/components/layouts/PuzzlePieceContainer.tsx
@@ -1,12 +1,17 @@
 'use client';
 
-import { Center } from '@chakra-ui/react';
+import { Center, Flex } from '@chakra-ui/react';
 import React from 'react';
+import { Piece } from '../elements/Puzzle/Piece';
 
-export default function PuzzlePieceContainer() {
+export const PuzzlePieceContainer = () => {
   return (
     <Center height={100} bgColor="#D9D9D9">
-      PuzzlePieceContainer
+      <Flex gap={4}>
+        <Piece />
+        <Piece />
+        <Piece />
+      </Flex>
     </Center>
   );
-}
+};
