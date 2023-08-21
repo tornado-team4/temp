fc221dd1752c3f1bd1050a7e76faac5851adb21f
NewNomad <tosiki1997@gmail.com>
2023-08-21 13:26:06 +0900

    feat: 暫定css整え

--- frontend/src/app/ingame/components/layouts/ClientComponent.tsx
+++ frontend/src/app/ingame/components/layouts/ClientComponent.tsx
@@ -4,13 +4,23 @@ import React from 'react';
 import { PuzzleContainer } from './PuzzleContainer';
 import { InputMemoryContainer } from './InputMemoryContainer';
 import PuzzlePieceContainer from './PuzzlePieceContainer';
+import { Grid } from '@chakra-ui/react';
 
 export function ClientComponent() {
   return (
     <>
       <PuzzleContainer />
-      <InputMemoryContainer />
-      <PuzzlePieceContainer />
+      <Grid
+        templateColumns="7fr 5fr"
+        position="fixed"
+        bottom="0"
+        left={0}
+        right={0}
+        gap={0}
+      >
+        <InputMemoryContainer />
+        <PuzzlePieceContainer />
+      </Grid>
     </>
   );
 }
--- frontend/src/app/ingame/components/layouts/InputMemoryContainer.tsx
+++ frontend/src/app/ingame/components/layouts/InputMemoryContainer.tsx
@@ -1,7 +1,19 @@
 'use client';
 
+import { Button, Center, Flex, Input } from '@chakra-ui/react';
 import React from 'react';
 
 export function InputMemoryContainer() {
-  return <div>InputMemoryContainer</div>;
+  return (
+    <Center height={100} bgColor="#78CDFD">
+      <Flex gap={5}>
+        <Input
+          placeholder="この写真へ思い出の一言"
+          bgColor="whiteAlpha.900"
+          width={500}
+        />
+        <Button>Submit</Button>
+      </Flex>
+    </Center>
+  );
 }
