cac7b1797a9658a114e08b4a03727d6f1c1bc8a2
NewNomad <tosiki1997@gmail.com>
2023-08-21 18:02:52 +0900

    feat: ピースに画像をセットする機能を追加

--- frontend/src/app/ingame/components/layouts/ClientComponent.tsx
+++ frontend/src/app/ingame/components/layouts/ClientComponent.tsx
@@ -3,8 +3,8 @@
 import React from 'react';
 import { PuzzleContainer } from './PuzzleContainer';
 import { InputMemoryContainer } from './InputMemoryContainer';
-import PuzzlePieceContainer from './PuzzlePieceContainer';
 import { Grid } from '@chakra-ui/react';
+import { PuzzlePieceContainer } from './PuzzlePieceContainer';
 
 export function ClientComponent() {
   return (
