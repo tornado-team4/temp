068d11851304c772d63cbecbaaf3022c23e75d0d
NewNomad <tosiki1997@gmail.com>
2023-08-21 13:37:14 +0900

    feat: 見やすくなるようIcon追加

--- frontend/src/app/ingame/components/layouts/InputMemoryContainer.tsx
+++ frontend/src/app/ingame/components/layouts/InputMemoryContainer.tsx
@@ -1,18 +1,31 @@
 'use client';
 
-import { Button, Center, Flex, Input } from '@chakra-ui/react';
+import { EditIcon, EmailIcon } from '@chakra-ui/icons';
+import {
+  Button,
+  Center,
+  Flex,
+  Input,
+  InputGroup,
+  InputLeftElement,
+} from '@chakra-ui/react';
 import React from 'react';
 
 export function InputMemoryContainer() {
   return (
     <Center height={100} bgColor="#78CDFD">
       <Flex gap={5}>
-        <Input
-          placeholder="この写真へ思い出の一言"
-          bgColor="whiteAlpha.900"
-          width={500}
-        />
-        <Button>送る</Button>
+        <InputGroup width={500}>
+          <InputLeftElement>
+            <EditIcon />
+          </InputLeftElement>
+          <Input
+            bgColor="whiteAlpha.900"
+            placeholder="この写真へ思い出の一言"
+          />
+        </InputGroup>
+
+        <Button rightIcon={<EmailIcon />}>送る</Button>
       </Flex>
     </Center>
   );
