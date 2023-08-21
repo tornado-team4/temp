1a1035d9bb6187cbcffac6b080650af547d1de99
NewNomad <tosiki1997@gmail.com>
2023-08-21 18:03:06 +0900

    feat: ピースに画像をセットする機能を追加

--- frontend/src/app/ingame/components/elements/Puzzle/Piece.tsx
+++ frontend/src/app/ingame/components/elements/Puzzle/Piece.tsx
@@ -2,29 +2,50 @@
 
 import React from 'react';
 import { motion } from 'framer-motion';
-import { log } from 'console';
 
 type Props = {
+  index?: number;
   isCompleted?: boolean;
 };
 
-export const Piece = ({ isCompleted }: Props) => {
-  log(isCompleted);
+export const Piece = ({ index, isCompleted }: Props) => {
+  if (index === 0 && isCompleted === true) {
+    console.log(1);
+  }
+
   return (
     <motion.div
       drag
       dragSnapToOrigin
       dragElastic={1}
-      dragTransition={{ bounceStiffness: 100, bounceDamping: 12 }}
-      whileHover={{ scale: 1.1 }}
+      dragTransition={{ bounceStiffness: 150, bounceDamping: 15 }}
+      whileHover={{ cursor: 'grabbing' }}
+      whileTap={{ cursor: 'grabbing' }}
       whileDrag={{ scale: 1 }}
       style={{
         backgroundColor: 'red',
+        position: 'relative',
         width: 100,
         height: 100,
+        backgroundImage: 'url(/dummy/dummy2.jpg)',
+        backgroundSize: 1000,
+        backgroundPosition: '50% 50%',
       }}
     >
-      aa
+      {/* <Image
+        src="/dummy/dummy2.jpg"
+        alt="1"
+        width={200}
+        height={200}
+        draggable={false}
+        style={{
+          // position: 'absolute',
+          clipPath: 'inset(0px 0px 0px 0px)',
+          // width: 200,
+          // height: 200,
+          objectFit: 'cover',
+        }}
+      /> */}
     </motion.div>
   );
 };
