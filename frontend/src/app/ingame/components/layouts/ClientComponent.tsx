4c42e2fb7ac778cc7b43a733fdabe90baf271416
NewNomad <tosiki1997@gmail.com>
2023-08-21 12:24:38 +0900

    feat: 必要なorganismsを追加

--- frontend/src/app/ingame/components/layouts/ClientComponent.tsx
+++ frontend/src/app/ingame/components/layouts/ClientComponent.tsx
@@ -1,7 +1,16 @@
 'use client';
 
 import React from 'react';
+import { PuzzleContainer } from './PuzzleContainer';
+import { InputMemoryContainer } from './InputMemoryContainer';
+import PuzzlePieceContainer from './PuzzlePieceContainer';
 
 export function ClientComponent() {
-  return <div>ClientComponent</div>;
+  return (
+    <>
+      <PuzzleContainer />
+      <InputMemoryContainer />
+      <PuzzlePieceContainer />
+    </>
+  );
 }
