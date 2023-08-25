f41834ff4c7d9414e3144906f35599c824b26cb3
tamaki8021 <hbk2002320@gmail.com>
2023-08-26 01:38:19 +0900

    feat: トップページ全体のUIの組み上げ

--- frontend/src/app/top/page.tsx
+++ frontend/src/app/top/page.tsx
@@ -1,16 +1,47 @@
 'use client';
 
-import { HStack, Box } from '@chakra-ui/react';
+import { Stack, Box, VStack, Image } from '@chakra-ui/react';
 import { JoinRoomArea } from '@/app/top/components/JoinRoomArea';
-import { HowToPlayArea } from './components/HowToPlayArea';
+import { HowToPlayArea } from '@/app/top/components/HowToPlayArea';
+import logo from '/public/logo.png';
+import bg_img from '/public/bg_img.jpeg';
 
 export default function Top() {
   return (
-    <HStack justifyContent="center">
-      <JoinRoomArea />
-      <Box w="50%">
-        <HowToPlayArea />
+    <Box
+      w="full"
+      h="full"
+      minH="100vh"
+      bgImage={bg_img.src}
+      objectFit="cover"
+      p={8}
+    >
+      <Box
+        w="full"
+        h="full"
+        minH="100vh"
+        bg="whiteAlpha.500"
+        borderRadius={10}
+        py={10}
+        px={4}
+      >
+        <VStack h="full" w="full">
+          <Image src={logo.src} alt="青春パズルロゴ" />
+          <Box h={{ base: 14, md: 20 }} />
+          <Stack
+            direction={{ base: 'column', md: 'row' }}
+            w="full"
+            justifyContent="center"
+          >
+            <Box w={{ base: 'full', md: '50%' }}>
+              <JoinRoomArea />
+            </Box>
+            <Box w={{ base: 'full', md: '50%' }}>
+              <HowToPlayArea />
+            </Box>
+          </Stack>
+        </VStack>
       </Box>
-    </HStack>
+    </Box>
   );
 }
