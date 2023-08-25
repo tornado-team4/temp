0ed72e4fc71b2b1a8812475f92cc152a64be8d13
ltoppyl <toppyajibutu@gmail.com>
2023-08-25 22:47:20 +0900

    feat: Firebase と画像のやりとりをする諸々

---
+++ frontend/src/firebase/uploadImage.ts
@@ -0,0 +1,19 @@
+import { ref, uploadBytes } from 'firebase/storage';
+import { generateRandomString } from '@/utils/generateRandomString';
+import { storage } from '../../firebase'; // FIXME: 絶対パスで指定するようにする
+
+export const uploadImage = async (file: File) => {
+  let fileName = generateRandomString(30);
+
+  const storageRef = ref(storage, fileName);
+
+  await uploadBytes(storageRef, file)
+    .then(() => {
+      // do nothing
+    })
+    .catch(() => {
+      fileName = ''; // 送信に失敗した場合は空文字を返す
+    });
+
+  return fileName;
+};
