bcf54e31678d9ca3e5c9e09b23ef95952d0d6c15
tamaki8021 <hbk2002320@gmail.com>
2023-08-26 03:06:50 +0900

    refactor: Avatar componentがstorybook上で動くように変更

--- frontend/src/app/top/components/Avatar/index.stories.tsx
+++ frontend/src/app/top/components/Avatar/index.stories.tsx
@@ -10,9 +10,17 @@ const meta: Meta<typeof Avatar> = {
 
 export default meta;
 type Story = StoryObj<typeof Avatar>;
-
+const dummyAvatarImgList = [
+  'https://bit.ly/dan-abramov',
+  'https://bit.ly/kent-c-dodds',
+  'https://bit.ly/ryan-florence',
+  'https://bit.ly/prosper-baba',
+  'https://bit.ly/code-beast',
+];
 export const Default: Story = {
-  args: {},
+  args: {
+    avatarList: dummyAvatarImgList,
+  },
 };
 
 export const Md: Story = {
