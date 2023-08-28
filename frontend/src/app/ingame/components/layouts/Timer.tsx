3ae957081cd6c2c2b432d4726f33e9c6104de475
NewNomad <tosiki1997@gmail.com>
2023-08-28 16:18:06 +0900

    feat: CountDownTimerコンポーネント

--- frontend/src/app/ingame/components/layouts/Timer.tsx
+++ frontend/src/app/ingame/components/layouts/Timer.tsx
@@ -1,5 +1,15 @@
 'use client';
+import { CountdownCircleTimer } from 'react-countdown-circle-timer';
 
 export const Timer = () => {
-  return <div>Timer</div>;
+  return (
+    <CountdownCircleTimer
+      isPlaying
+      duration={8}
+      colors={['#004777', '#f7b801', '#A30000', '#ff6363']}
+      colorsTime={[9, 8, 7, 6]}
+    >
+      {({ remainingTime }) => remainingTime}
+    </CountdownCircleTimer>
+  );
 };
