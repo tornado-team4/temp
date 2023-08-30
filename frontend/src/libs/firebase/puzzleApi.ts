2066e391c333029ed4dd02bff2334a6e149f7e21
NewNomad <tosiki1997@gmail.com>
2023-08-30 15:13:36 +0900

    feat: パズル用firebase通信ファイル作成

---
+++ frontend/src/libs/firebase/puzzleApi.ts
@@ -0,0 +1,9 @@
+// import { storage } from '@/libs/firebase/firebase';
+
+export const puzzleApi = () => {
+  const updatePieces = () => {};
+
+  const createMemories = async () => {};
+
+  return { updatePieces, createMemories };
+};
