ad7714efd82a25b0edf17c6778dda53f1c7e73cc
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2023-08-31 08:48:27 +0900

    fix: 諸々修正してみる

--- frontend/src/utils/baseUrl.ts
+++ frontend/src/utils/baseUrl.ts
@@ -1,4 +1,9 @@
-export const BASE_URL = 'https://tornado-team4-be.vercel.app';
-// process.env.NODE_ENV === 'development'
-//   ? 'http://localhost:8080'
-//   : 'https://tornado-team4-be.vercel.app';
+export const BASE_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8080'
+    : 'https://tornado-team4-be.vercel.app';
+
+export const WS_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'ws://localhost:8080'
+    : 'wss://tornado-team4-be.vercel.app';
