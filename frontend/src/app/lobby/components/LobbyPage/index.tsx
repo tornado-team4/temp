d5c1a8535b778bb36645874e86e89f51bae70245
tamaki8021 <hbk2002320@gmail.com>
2023-08-31 02:10:00 +0900

    feat: ロビー画面のUIの組み上げ

---
+++ frontend/src/app/lobby/components/LobbyPage/index.tsx
@@ -0,0 +1,68 @@
+'use client';
+
+import { VSpacer } from '@/components/Spacer';
+import { Box, VStack, Image, Stack, HStack } from '@chakra-ui/react';
+import { LinkIcon } from '@chakra-ui/icons';
+import { BiSolidRightArrow } from 'react-icons/bi';
+import bg_img from 'public/bg_img.jpeg';
+import logo from 'public/logo.png';
+import { PlayerList } from '@/app/lobby/components/PlayerList';
+import { PuzzleMaker } from '@/app/lobby/components/PuzzleMaker';
+import { OutlineButtonWithRightIcon } from '@/components/Button/OutlineButtonWithRightIcon';
+
+export const LobbyPage = () => {
+  return (
+    <Box
+      w="full"
+      h="full"
+      minH="100vh"
+      bgImage={bg_img.src}
+      objectFit="cover"
+      p={8}
+    >
+      <Box
+        w="full"
+        h="full"
+        minH="100vh"
+        bg="whiteAlpha.500"
+        borderRadius={10}
+        py={10}
+        px={4}
+      >
+        <VStack h="full" w="full">
+          <Image src={logo.src} alt="青春パズルロゴ" />
+          <VSpacer size={12} />
+          <Stack direction={{ base: 'column', md: 'row' }} w="full">
+            <Box w={{ base: 'full', md: '50%' }}>
+              <PlayerList list={[]} />
+            </Box>
+            <Box w={{ base: 'full', md: '50%' }}>
+              <VStack h="full" w="full">
+                <PuzzleMaker />
+                <VSpacer size={8} />
+                <HStack w="full">
+                  <OutlineButtonWithRightIcon
+                    height="70px"
+                    text={'招待する'}
+                    rightIcon={<LinkIcon />}
+                    color={'#56C1FC'}
+                    bgColor={'white'}
+                    isDisabled={false}
+                  />
+                  <OutlineButtonWithRightIcon
+                    height="70px"
+                    text={'開始'}
+                    rightIcon={<BiSolidRightArrow />}
+                    color={'#56C1FC'}
+                    bgColor={'white'}
+                    isDisabled={false}
+                  />
+                </HStack>
+              </VStack>
+            </Box>
+          </Stack>
+        </VStack>
+      </Box>
+    </Box>
+  );
+};
---
+++ frontend/src/app/lobby/page.tsx
@@ -0,0 +1,5 @@
+import { LobbyPage } from '@/app/lobby/components/LobbyPage';
+
+export default function Lobby() {
+  return <LobbyPage />;
+}
