af0f17fdb36e727b57829b6ed3f5e3c8dcb6d5eb
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2023-08-31 21:00:22 +0900

    feat: モバイル用のページの作成

---
+++ frontend/src/app/top/components/Mobile/index.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { Mobile } from './index';
+
+const meta: Meta<typeof Mobile> = {
+  title: 'TopPage/Mobile',
+  component: Mobile,
+};
+
+export default meta;
+type Story = StoryObj<typeof Mobile>;
+
+export const Default: Story = {
+  args: {
+    bgColor: 'blue',
+  },
+};
--- frontend/src/app/top/page.tsx
+++ frontend/src/app/top/page.tsx
@@ -1,7 +1,10 @@
 'use client';
-
+import { useBreakpointValue } from '@chakra-ui/react';
 import { TopPage } from './components/TopPage';
+import { Mobile } from '@/app/top/components/Mobile';
 
 export default function Top() {
-  return <TopPage roomId="" />;
+  const mediaType = useBreakpointValue({ base: 'phone', md: 'pc' });
+
+  return mediaType === 'pc' ? <TopPage roomId="" /> : <Mobile />;
 }
