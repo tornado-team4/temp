4b79c711786a091feaa75c41006ff1409f7c5f78
NewNomad <tosiki1997@gmail.com>
2023-08-31 10:17:58 +0900

    feat: userのグローバルステート

---
+++ frontend/src/app/firebase/atoms/userState.ts
@@ -0,0 +1,12 @@
+import { atom } from 'recoil';
+import { User } from '@/types/User';
+
+export const userState = atom<User>({
+  key: 'userState',
+  default: {
+    id: '',
+    name: '',
+    avatarUrl: '',
+    role: '',
+  },
+});
