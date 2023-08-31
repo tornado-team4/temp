b5e50d396f01417b7505c4a770f08a144fc88c9b
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2023-08-31 18:06:52 +0900

    feat: / から top への遷移の作成

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,13 +1,15 @@
 'use client';
 
-import { Link } from '@chakra-ui/next-js';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function Home() {
-  return (
-    <>
-      <Link href="/" color="blue" _hover={{ color: 'blue.500' }}>
-        aaaa
-      </Link>
-    </>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    router.replace('/top');
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return <></>;
 }
