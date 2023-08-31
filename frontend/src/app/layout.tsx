9a9bda9b163f87d7c2dadde92189b6690cb93985
NewNomad <tosiki1997@gmail.com>
2023-08-31 10:18:57 +0900

    feat: RecoilRootでラップ

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Providers from '@/app/providers';
+import { RecoilRoot } from 'recoil';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,7 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+        <Providers>
+          <RecoilRoot>{children}</RecoilRoot>
+        </Providers>
       </body>
     </html>
   );
