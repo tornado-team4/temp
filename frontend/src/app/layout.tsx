192599b67cdc4c400e42c6604b659ca29dd2fc7c
NewNomad <tosiki1997@gmail.com>
2023-08-31 10:23:05 +0900

    fix: クライアント側でrecoilの状態を持たせるため

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import Providers from '@/app/providers';
-import { RecoilRoot } from 'recoil';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -18,9 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Providers>
-          <RecoilRoot>{children}</RecoilRoot>
-        </Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
