24e55030dc7da6b33dab02fd528948192164ed54
NewNomad <tosiki1997@gmail.com>
2023-08-31 10:38:42 +0900

    feat: コメントと写真のコンポーネント

--- frontend/src/app/album/components/layout/ClientComponent.tsx
+++ frontend/src/app/album/components/layout/ClientComponent.tsx
@@ -1,5 +1,22 @@
 'use client';
 
+import { Box } from '@chakra-ui/react';
+import bg_img from '/public/bg_img.jpeg';
+import { Comments } from './Comments';
+import { Photos } from './Photos';
+
 export function ClientComponent() {
-  return <div>ClientComponent</div>;
+  return (
+    <Box
+      w="full"
+      h="full"
+      minH="100vh"
+      bgImage={bg_img.src}
+      objectFit="cover"
+      p={8}
+    >
+      <Comments />
+      <Photos />
+    </Box>
+  );
 }
