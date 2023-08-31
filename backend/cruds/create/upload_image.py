45617efa947ab9494f2447bcd93b9159fe7df83d
ltoppyl <toppiiajibutu2@yahoo.co.jp>
2023-08-31 13:22:35 +0900

    fix: 軽微な修正

--- backend/cruds/create/upload_image.py
+++ backend/cruds/create/upload_image.py
@@ -3,7 +3,7 @@ def upload_image(db, room_id, url):
     images_doc_ref = images_collection_ref.document()
     images_doc_ref.set({"url": url})
 
-    images_collection_ref = db.collection("room").document(room_id).collection("gameObjects").document("Image")
+    images_doc_ref = db.collection("room").document(room_id).collection("gameObjects").document("Image")
     images_doc_ref.set({"url": url})
 
     return "success"
